b20fab97cf92bdea52cd09c1df04fc5426241e69
Jan Bogaerts <jb@allthingstalk.com>
2016-09-12 18:01:39 +0200

    init checkin

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pygate_groups',
+    version='1.0',
+    packages=['pygate_groups'],
+    url='www.allthingstalk.com',
+    license='',
+    author='Jan Bogaerts',
+    author_email='jb@allthingstalk.com',
+    description="plugin for the pygate: group assets together and use them as 1"
+)
